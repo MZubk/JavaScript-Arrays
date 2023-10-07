2e7977b9a60beb88547f7d6a3a9ff434acd06a5c
Murillo Zubk <mzubk.dev@gmail.com>
2023-10-06 21:54:26 -0300

    procurando o aluno

---
+++ tests/concatenando.js
@@ -0,0 +1,28 @@
+// const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"];
+// const arrayConcat = arrayOriginal.concat("André", "Fernanda");
+
+// console.log(arrayConcat);
+// console.log(arrayOriginal);
+
+// const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"];
+// const arrayConcat = arrayOriginal.concat(
+//   ["André", "Fernanda"],
+//   ["Ricardo", "Ana"],
+//   ["Marcelo", "Bia"]
+// );
+
+// console.log(arrayConcat);
+// console.log(arrayOriginal);
+
+// const arrayOriginal = [50, 60, 70];
+// const arrayConcat = arrayOriginal.concat([80, [90, 100]]);
+
+// console.log(arrayConcat);
+// console.log(arrayOriginal);
+
+const funcionarios = [
+  ["Ana", "Juliana", "Leonardo"],
+  [30, 35, 28],
+];
+
+console.log(`${funcionarios[0][2]} tem idade de ${funcionarios[1][2]}`);
