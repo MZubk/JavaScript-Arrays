ea9179b77ff9f7e1d91a4b326b79b29e11c24c9b
Murillo Zubk <mzubk.dev@gmail.com>
2023-10-17 19:01:21 -0300

    callbacks

--- map/script.js
+++ map/script.js
@@ -1,28 +1,31 @@
 const notas = [10, 9.5, 8, 7, 6];
 
-for (let i = 0; i < notas.length; i++) {
-  const atualizaNotas = notas[i] + 1;
-  notas[i] = atualizaNotas >= 10 ? 10 : atualizaNotas;
-}
+// for (let i = 0; i < notas.length; i++) {
+//   const atualizaNotas = notas[i] + 1;
+//   notas[i] = atualizaNotas >= 10 ? 10 : atualizaNotas;
+// }
 
-console.log(`notas atualizadas ${notas} 'for'`); // 10,10,9,8,7
+// console.log(`notas atualizadas ${notas} 'for'`); // 10,10,9,8,7
+
+const notasAtualizadas = [];
 
 for (let nota of notas) {
   const atualizaNotas = nota + 1;
-  const notasAtualizadas = atualizaNotas >= 10 ? 10 : atualizaNotas;
+  const notasAtualizada = atualizaNotas >= 10 ? 10 : atualizaNotas;
+  notasAtualizadas.push(notasAtualizada);
 }
 
-console.log(`notas atualizadas ${notas} 'for ...of'`); // 10,10,9,8,7
+console.log(`notas atualizadas ${notasAtualizadas} 'for ...of'`); // 10,10,9,8,7
 
-notas.forEach(function (nota, index, notas) {
-  const atualizaNotas = nota + 1;
-  notas[index] = atualizaNotas >= 10 ? 10 : atualizaNotas;
-});
+// notas.forEach(function (nota, index, notas) {
+//   const atualizaNotas = nota + 1;
+//   notas[index] = atualizaNotas >= 10 ? 10 : atualizaNotas;
+// });
 
-console.log(`notas atualizadas ${notas} 'forEach'`); // 10,10,9,8,7
+// console.log(`notas atualizadas ${notas} 'forEach'`); // 10,10,9,8,7
 
-const atualizaNotas = notas.map((nota) => {
-  return nota + 1 >= 10 ? 10 : nota + 1;
-});
+// const atualizaNotas = notas.map((nota) => {
+//   return nota + 1 >= 10 ? 10 : nota + 1;
+// });
 
-console.log(`notas atualizadas ${atualizaNotas} 'map'`); // 10,10,9,8,7
+// console.log(`notas atualizadas ${atualizaNotas} 'map'`); // 10,10,9,8,7
